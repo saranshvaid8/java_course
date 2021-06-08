14745946677afe29343a472158976b0d806eff21
saransh vaid <saransh.vaid@transflo.com>
2021-06-08 17:41:18 -0400

    adding GrossPayCalculator class

--- src/chaptertwo/GrossPayCalculator.java
+++ src/chaptertwo/GrossPayCalculator.java
@@ -1,4 +1,37 @@
 package chaptertwo;
 
+import java.util.Scanner;
+
 public class GrossPayCalculator {
+
+    public static void main(String arg []){
+        Scanner scanner = new Scanner(System.in);
+        GrossPayCalculator grossPayCalculator = new GrossPayCalculator();
+         //* Get the number of hours worked
+        int numberOfHours = grossPayCalculator.getNumberOfHours(scanner);
+        //* Get the hourly pay rate
+        double rate = grossPayCalculator.getRate(scanner);
+        //* Multiple hours and pay rate
+        grossPayCalculator.calculateSalary(scanner, numberOfHours, rate);
+
+    }
+
+    private void calculateSalary(Scanner scanner, int numberOfHours, double rate) {
+        double salary = numberOfHours * rate;
+        scanner.close();
+        //* Display result
+        System.out.println("You salary is = " + salary + "$");
+    }
+
+    private int getNumberOfHours(Scanner scanner) {
+        System.out.println("Enter the number of hours the employee worked");
+        int numberOfHours = scanner.nextInt();
+        return numberOfHours;
+    }
+
+    private double getRate(Scanner scanner) {
+        System.out.println("Enter the employee's pay rate");
+        double rate = scanner.nextDouble();
+        return rate;
+    }
 }
